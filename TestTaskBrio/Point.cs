5d3ba2acb63d5d2d6fb92fd0c6aee109d3f17ece
Alliques <airat.murtazin@intravision.ru>
2022-03-20 16:38:41 +0300

    remastered

---
+++ TestTaskBrio/Point.cs
@@ -0,0 +1,17 @@
+﻿namespace TestTaskBrio
+{
+    public class Marker
+    {
+        public float X { get; set; }
+        public float Y { get; set; }
+        public int Radius { get; set; }
+        public NeighborMarker PreviousMarker { get; set; }
+        public NeighborMarker NextMarker { get; set; }
+    }
+
+    public class NeighborMarker
+    {
+        public float X { get; set; }
+        public float Y { get; set; }
+    }
+}
--- TestTaskBrio/Startup.cs
+++ TestTaskBrio/Startup.cs
@@ -1,16 +1,10 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.SpaServices.AngularCli;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using TestTaskBrio.Infrastructure.Data;
 
 namespace TestTaskBrio
 {
@@ -23,12 +17,25 @@ namespace TestTaskBrio
 
         public IConfiguration Configuration { get; }
 
+        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options => {
+                options.AddPolicy("CorsPolicy", builder => builder
+                .AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader());
+            });
             services.AddSignalR();
-            services.AddRazorPages();
+            services.AddControllersWithViews();
+            // In production, the Angular files will be served from this directory
+            services.AddSpaStaticFiles(configuration =>
+            {
+                configuration.RootPath = "ClientApp/dist";
+            });
         }
 
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -43,20 +50,35 @@ namespace TestTaskBrio
             }
 
             app.UseHttpsRedirection();
-            app.UseDefaultFiles();
             app.UseStaticFiles();
-
+            app.UseCors("CorsPolicy");
+            if (!env.IsDevelopment())
+            {
+                app.UseSpaStaticFiles();
+            }
+            app.UseDefaultFiles();
 
             app.UseRouting();
+
             app.UseEndpoints(endpoints =>
             {
+                //endpoints.MapControllerRoute(
+                //    name: "default",
+                //    pattern: "{controller}/{action=Index}/{id?}");
                 endpoints.MapHub<AppHub>("/points");
             });
 
-
-            app.UseEndpoints(endpoints =>
+            app.UseSpa(spa =>
             {
-                endpoints.MapRazorPages();
+                // To learn more about options for serving an Angular SPA from ASP.NET Core,
+                // see https://go.microsoft.com/fwlink/?linkid=864501
+
+                spa.Options.SourcePath = "ClientApp";
+
+                if (env.IsDevelopment())
+                {
+                    spa.UseAngularCliServer(npmScript: "start");
+                }
             });
         }
     }
